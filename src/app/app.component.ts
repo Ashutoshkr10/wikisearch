2c5a53f2c07b88dc8918afff37e14561b0bed444
Ashutoshkr10 <54498380+Ashutoshkr10@users.noreply.github.com>
2020-10-03 09:39:13 +0530

    mattable not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { SearchResult } from './search-result.model';
+import { WikiService } from './wiki.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'wiki-search-app';
+  pages: SearchResult[] = [];
+
+  constructor(private wikiService: WikiService) {}
+  onTerm(term: string) {
+    this.wikiService.search(term).subscribe((response: any) => {
+      this.pages = response.query.search;
+      console.log(this.pages);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
@@ -13,10 +15,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   declarations: [AppComponent, SearchBarComponent, PageListComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
     MatTableModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
